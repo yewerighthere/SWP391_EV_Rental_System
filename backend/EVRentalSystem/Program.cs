46759f645983e45cfdf8673c2bb31de262cf74b8
Gia Huy <onggiahuy222@gmail.com>
2025-10-08 21:23:35 +0700

    add: ConnectionString

--- backend/EVRentalSystem/Program.cs
+++ backend/EVRentalSystem/Program.cs
@@ -1,3 +1,6 @@
+using EVRentalSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
