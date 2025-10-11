68b07d2fc2fdc58c4e6a7ad4586e2234254c4207
Gia Huy <onggiahuy222@gmail.com>
2025-10-12 02:12:14 +0700

    remove: some stuff.

--- backend/EVRentalSystem/Program.cs
+++ backend/EVRentalSystem/Program.cs
@@ -8,8 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 
 // Configure the HTTP request pipeline.
